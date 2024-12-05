018662dfad73c1e8c37e9ae9a7f0af874330401d
Joseph halden <josephhalden1@gmail.com>
2024-12-05 18:32:59 +0000

    trying to run android studio

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
-// main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:19006', // Replace with the URL of your React Native development server
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Allow cookies if needed
+  });
+
+  await app.listen(3000); // Ensure the backend listens on the correct port
+}
 bootstrap();
